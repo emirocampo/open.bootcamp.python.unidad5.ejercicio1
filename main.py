30084898d22d0b07473fe126e53ccf0819f53943
emiro campo <emiroluiscampo@gmail.com>
2022-06-23 08:29:41 -0500

    completo

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+
+def areaTriangulo(base, altura):
+    return (base*altura/2)
+
+def areaCirculo(radio):
+    return(math.pi*radio**2)
+
+print(areaTriangulo(2,3))
+print(areaCirculo(2))
